1ad18915a369ad43e96f3ab4814f8e256f3fd706
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-11 12:42:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget(parent, child) {
+   return document.querySelector(`${parent} ${child}`);
+   
+   
+  
+}
+
+
+function increaseRankBy(n){
+  const rank = document.querySelectorAll('ul.ranked-list li');
+  for (let i=0; i < rank.length; i++) {
+    rank[i].textContent = parseInt(rank[i].textContent)+n;
+  }
+}
+
+
+function deepestChild() {
+  let parent = document.getElementById('grand-node');
+  
+  if(parent.children[0].textContent) {
+    return parent.children[0].textContent;
+  }
+  else if(parent.children[0][0].textContent) {
+    return parent.children[0][0].textContent;
+  }
+  else if (parent.children[0][0][0].textContent) {
+    return parent.children[0][0][0].textContent;
+  }
+  else if (parent.children[0][0][0][0].textContent) {
+    return parent.children[0][0][0][0].textContent;
+  }
+}
