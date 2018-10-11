582e714ed33c3e9080f4c0da6351361006a17995
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-11 12:58:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget(parent, child) {
-    return document.querySelector(parent, child);
+    return document.querySelector(parent.child);
     
     //let nested = document.getElementById(parent).getElementsByClassName(child);
     //return nested;
