2a7f89f20510a083162858f32f35a3c6e29a9fca
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-11 13:10:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+   
+    let nested = document.getElementById('nested').querySelector('.target');
+    return nested;
+}
+
+
+function increaseRankBy(n){
+  const rank = document.querySelectorAll('ul.ranked-list li');
+  for (let i=0; i < rank.length; i++) {
+    rank[i].textContent = parseInt(rank[i].textContent)+n;
+  }
+}
+
+
+function deepestChild() {
+  let parent = document.querySelector('#grand-node div div div div');
+ return parent; 
+}
